bc45a33fb0b4aa6395e258e53f46e1c2f9c1c4e0
Piotr Głąb <piotgla495@student.polsl.pl>
2024-02-02 23:57:21 +0100

    UGV-22 added choose file command for localizer view's background image loading

---
+++ UWB-Geolocation-Visualizer/MVVM/ViewModel/Commands/MainWindow/ChooseFileCommand.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Windows.Media.Imaging;
+using UWB_Geolocation_Visualizer.Core;
+
+namespace UWB_Geolocation_Visualizer.MVVM.ViewModel.Commands.MainWindow
+{
+    public class ChooseFileCommand : BaseCommand
+    {
+        private LocalizerViewModel localizerViewModel;
+
+        public ChooseFileCommand(LocalizerViewModel localizerViewModel)
+        {
+            this.localizerViewModel = localizerViewModel;
+        }
+
+        public override void Execute(object? parameter)
+        {
+            OpenFileDialog openFileDialog = new()
+            {
+                Filter = "Image Files (*.png;*.jpg;*.jpeg;*.bmp)|*.png;*.jpg;*.jpeg;*.bmp",
+                Title = "Wybierz tło obszaru lokalizacji"
+            };
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string selectedImagePath = openFileDialog.FileName;
+                BitmapImage bitmapImage = new(new Uri(selectedImagePath));
+                localizerViewModel.LocalizerBgSource = bitmapImage;
+            }
+        }
+    }
+}
