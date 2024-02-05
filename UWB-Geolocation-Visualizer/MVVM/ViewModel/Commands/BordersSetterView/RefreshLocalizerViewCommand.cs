b0c9876dd6464a999f2882276efbd524e2ad6b84
Piotr Głąb <piotgla495@student.polsl.pl>
2024-02-05 21:19:13 +0100

    removed unnecessary comment

--- UWB-Geolocation-Visualizer/MVVM/ViewModel/Commands/BordersSetterView/RefreshLocalizerViewCommand.cs
+++ UWB-Geolocation-Visualizer/MVVM/ViewModel/Commands/BordersSetterView/RefreshLocalizerViewCommand.cs
@@ -8,7 +8,7 @@ namespace UWB_Geolocation_Visualizer.MVVM.ViewModel.Commands.BordersSetterView
         private readonly BordersSetterViewModel model;
 
         public event Action RequestRefreshLocalizerView;
-        //todo: check because the border x max didn't change for a point last time...
+
         public override void Execute(object? parameter)
         {
             if (!double.TryParse(model.XBorderMinViewModel.Location, out double borderXMin))
